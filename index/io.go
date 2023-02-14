26d43f217f164879e462abc73822d814fdaaf533
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-02-14 21:43:26 +0530

    added crawlDirectory method

--- index/io.go
+++ index/io.go
@@ -1,5 +1,32 @@
 package index
 
+import (
+	"path/filepath"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	af "github.com/spf13/afero"
+)
+
+func crawlDirectory(directory string) []string {
+	files, err := af.ReadDir(I.FileSystem, directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := []string{}
+
+	for _, file := range files {
+		extension := filepath.Ext(file.Name())
+		if extension == ".json" {
+			name := strings.TrimSuffix(file.Name(), ".json")
+			res = append(res, name)
+		}
+	}
+
+	return res
+}
+
 // replaceContent changes the contents of file f to be str
 func (f *File) replaceContent(str string) error {
 	// write lock on file
