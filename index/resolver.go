ccf8f181ee36e1cc36b5a15949762384f282920c
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-02-15 22:23:37 +0530

    resolved the references

--- index/resolver.go
+++ index/resolver.go
@@ -2,6 +2,7 @@ package index
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -26,6 +27,53 @@ func resolveString(valString string, depthLeft int) interface{} {
 	return fmt.Sprintf("REF::ERR key '%s' not found", key)
 }
 
-func ResolveReferences(jsonVal interface{}, depthLen int) interface{} {
+// ResolveReferences tries to find key references and
+// if found, replace the references with their corresponding value
+func ResolveReferences(jsonVal interface{}, depthLeft int) interface{} {
+	// if max recursive depth is exceeded, return as is
+	if depthLeft < 1 {
+		return jsonVal
+	}
 
+	val := reflect.ValueOf(jsonVal)
+
+	switch val.Kind() {
+	case reflect.String:
+		valString := val.String()
+
+		// if value is reference to another key
+		if strings.Contains(valString, "REF::") {
+			resolvedString := resolveString(valString, depthLeft)
+			return resolvedString
+		}
+
+		// if not a ref, keep as it is
+		return valString
+
+	case reflect.Slice:
+		numberOfValues := val.Len()
+		newSlice := make([]interface{}, numberOfValues)
+
+		// for each value in the slice, try to resolve it recursively
+		for i := 0; i < numberOfValues; i++ {
+			pointer := val.Index(i)
+			newSlice[i] = ResolveReferences(pointer.Interface(), depthLeft)
+		}
+
+		return newSlice
+
+	case reflect.Map:
+		newMap := make(map[string]interface{})
+
+		// for each value in the map, try to resolve it recursively
+		for _, key := range val.MapKeys() {
+			nestedVal := val.MapIndex(key).Interface()
+			newMap[key.String()] = ResolveReferences(nestedVal, depthLeft)
+		}
+
+		return newMap
+
+	default:
+		return jsonVal
+	}
 }
